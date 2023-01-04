182751566c7db2d7f4bd30ce336b80437eb70f59
Albert García Cortés <albert@MacBook-Air-de-Albert.local>
2023-01-04 19:52:15 +0100

    firts commit

---
+++ AroundMe-front/AroundMe_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundMe_frontApp.swift
+//  AroundMe-front
+//
+//  Created by Albert García Cortés on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AroundMe_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
